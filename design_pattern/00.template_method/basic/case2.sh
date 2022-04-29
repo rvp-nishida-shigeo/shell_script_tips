54ef0372d98fffeda5f1c2d1e824a548ed3225fc
Shigeo NISHIDA <shigeo.nsd@gmail.com>
2022-04-29 17:12:24 +0900

    不要コードの削除

--- design_pattern/00.template_method/basic/case2.sh
+++ design_pattern/00.template_method/basic/case2.sh
@@ -2,12 +2,6 @@
 set -u -e 
 source ./abstract_case.sh
 
-function called() {
-    local funcname="${FUNCNAME[1]}";
-    echo "INFO: called ${funcname} ";
-    return 0;
-}
-
 function foo() {
     let val=${1}+1;
     echo "${1} + 1 = ${val}";
