f43fb64ea3d6f487ad027cdf8ffdafbee6d0e73a
Shigeo Nishida <nishida.shigeo@revamp.co.jp>
2022-05-23 18:07:55 +0900

    コメント追加。

--- auto_tmpdir/auto_tmpdir.sh
+++ auto_tmpdir/auto_tmpdir.sh
@@ -20,4 +20,8 @@ function auto_tmpdir() {
     return $retval;
 }
 
+# NOTICE:
+# SIGHUP、SIGINT、SIGQUIT、SIGTERM を受けたときに true コマンドを実行する。
+# これらのシグナルを受けたときには何もせず、子プロセスに受けたシグナルを伝播させる。
+# 結果的に自プロセスは子プロセスの終了を待ち、__rm_tmpdir を実行することになる。
 trap true 1 2 3 15
